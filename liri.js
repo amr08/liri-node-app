f6f65d9c755df26b552e9d92451f63ad00c0fa29
Andrea R <amrdev08@gmail.com>
2016-09-18 15:45:57 -0400

    YAASSS

--- liri.js
+++ liri.js
@@ -4,14 +4,13 @@
   var Twitter = require('twitter');
   var userInput = process.argv[2];
 
-//TWITTER
-//doesn't work
-  var twits = keys.twitterKeys;
 
+//TWITTER
+  var twits = keys.twitterKeys;
   var client = new Twitter(twits);
-      console.log(client)
 
   var params = {screen_name: 'amrdev08'};
+
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
           if (!error) {
             }
