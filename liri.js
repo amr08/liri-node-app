4838cae3f117b4ca5439d5aad69aec2b61f3fd23
Andrea R <amrdev08@gmail.com>
2016-09-19 00:01:10 -0400

    started OMDB

--- liri.js
+++ liri.js
@@ -6,7 +6,7 @@
 
   var Twitter = require('twitter');
   var spotify = require('spotify');
-  // var request = require("request");
+  var request = require("request");
 
 
 //TWITTER
@@ -17,7 +17,7 @@
 
       client.get('statuses/user_timeline', params, function(error, tweets, response) {
           if (!error) {
-            }
+             }
 
           if (userInput == "my-tweets") {
                console.log(tweets)
@@ -25,49 +25,60 @@
       });
 
 
-
-
 //SPOTIFY
 
-    // function spotifySearch() {  spotify.search({ type: 'track', query: userSpotify }, function(err, data) {
-    //      if ( err ) {
-    //          console.log('Error occurred: ' + err);
-    //          return;
-    //        }
-    //
-    //      };
-      if (userInput == "spotify-this-song" && userSpotify) {
-              spotify.search({ type: 'track', query: userSpotify }, function(err, data) {
-                 if ( err ) {
-                     console.log('Error occurred: ' + err);
-                     return;
-                   }
+  if (userInput == "spotify-this-song" && userSpotify) {
+      spotify.search({ type: 'track', query: userSpotify }, function(err, data) {
+          if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+             }
 
-                  else {
+          else {
+                console.log("Artist: " + data.tracks.items[0].artists[0].name);
+                console.log("Song Title: " + data.tracks.items[0].name);
+                console.log("Link to Song: " + data.tracks.items[0].href);
+                console.log("Album: " + data.tracks.items[0].album.name);
+             }
+      });
+    }
 
-                    console.log("Artist: " + data.tracks.items[0].artists[0].name);
-                          console.log("Song Title: " + data.tracks.items[0].name);
-                          console.log("Link to Song: " + data.tracks.items[0].href);
-                          console.log("Album: " + data.tracks.items[0].album.name);
-                  }
-            });
-          }
+    else if (userInput == "spotify-this-song" && "undefined") {
+        var userSpotify = "the sign";
+        spotify.search({ type: 'track', query: userSpotify }, function(err, data) {
+            if ( err ) {
+                console.log('Error occurred: ' + err);
+                return;
+              }
 
-          else if (userInput == "spotify-this-song" && "undefined") {
-               var userSpotify = "the sign";
-                   spotify.search({ type: 'track', query: userSpotify }, function(err, data) {
-                      if ( err ) {
-                          console.log('Error occurred: ' + err);
-                          return;
-                        }
+            else {
+                console.log("Artist: " + data.tracks.items[6].artists[0].name)
+                console.log("Song Title: " + data.tracks.items[6].name);
+                console.log("Link to Song: " + data.tracks.items[6].href);
+                console.log("Album: " + data.tracks.items[6].album.name);
+              }
 
-                       else {
+        });
+    };
 
-                           console.log("Artist: " + data.tracks.items[6].artists[0].name)
-                           console.log("Song Title: " + data.tracks.items[6].name);
-                           console.log("Link to Song: " + data.tracks.items[6].href);
-                           console.log("Album: " + data.tracks.items[6].album.name);
-                       }
+//IMDB
 
-                   });
-              };
+    var queryUrl = "http://www.omdbapi.com/?t=" + userInput + "&y=&plot=short&r=json";
+
+      request(queryUrl, function(error, response, body) {
+
+	         if(!error && response.statusCode == 200) {
+
+
+	            console.log("Title: " + JSON.parse(body)["Title"]);
+              console.log("Year: " + JSON.parse(body)["Year"]);
+              console.log("Country: " + JSON.parse(body)["Country"]);
+              console.log("Language: " + JSON.parse(body)["Language"]);
+              console.log("Plot: " + JSON.parse(body)["Plot"]);
+              console.log("Actors: " + JSON.parse(body)["Actors"])
+
+              //  		* Rotten Tomatoes Rating.
+              //  		* Rotten Tomatoes URL.
+              }
+
+          });
